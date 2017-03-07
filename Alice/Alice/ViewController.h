6326282d0a52ecd9e89259e1ac404bf5d7a36f92
dx <dx@MiracleDebrisdeMacBook-Pro.local>
2017-03-07 20:51:10 +0800

    init

---
+++ Alice/Alice/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Alice
+//
+//  Created by dx on 17/3/7.
+//  Copyright © 2017年 dx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Alice/Alice/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Alice
+//
+//  Created by dx on 17/3/7.
+//  Copyright © 2017年 dx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
