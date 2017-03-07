85d82e5028236cff5e42a9744de8394ac4ba393f
dx <1352935273@qq.com>
2017-03-07 21:00:33 +0800

    Add Alice animation

--- Alice/Alice/ViewController.m
+++ Alice/Alice/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <Lottie/Lottie.h>
 
 @interface ViewController ()
 
@@ -16,14 +17,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.view.backgroundColor = [UIColor blackColor];
+    
+    LOTAnimationView *anim = [LOTAnimationView animationNamed:@"Alice"];
+    anim.bounds = self.view.bounds;
+    anim.center = self.view.center;
+    anim.contentMode = UIViewContentModeScaleAspectFill;
+    anim.loopAnimation = YES;
+    [self.view addSubview:anim];
+    
+    [anim play];
 }
 
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
 
 
 @end
